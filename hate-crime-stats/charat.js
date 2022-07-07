34825ec18d5c020d829b3904eb96c125fab9d2a4
Eric Lake <ericallenlake@gmail.com>
2022-07-07 15:06:44 -0600

    Starting to build count function

--- hate-crime-stats/charat.js
+++ hate-crime-stats/charat.js
@@ -1,7 +1,32 @@
 import * as d3 from "d3";
 
 async function drawLineChart() {
-    const dataset = await d3.csv('./data/BiasMotivatedCrimes.csv')
-
+    
+    const countPerDay = (data) => {
+        const arr = [];
+        let currentYear = 2000
+        let count = 1;
+        let tempObj = {
+            year: currentYear,
+            count: 1
+        }
+        data.forEach(object => {
+            if (object.OCC_YEAR > currentYear){
+                tempObj.count = count
+                arr.push(tempObj)
+                count = 1;
+                currentYear = object.OCC_YEAR;
+            } else {
+                count++;
+                console.log({count, currentYear})
+            }
+        }) 
+        return arr
+        // countPerDay(dataset)
+    }
+    const dataset = await d3.csv("./data/BiasMotivatedCrimes.csv")
     console.log(dataset)
+    console.log(countPerDay(dataset))
 }
+
+drawLineChart();
