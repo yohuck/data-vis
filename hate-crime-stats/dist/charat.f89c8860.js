1c3f356021684e29db2d9969ac242ad387bf89ab
Eric Lake <ericallenlake@gmail.com>
2022-07-08 21:22:33 -0600

    Adds wine file structure

--- hate-crime-stats/dist/charat.f89c8860.js
+++ hate-crime-stats/dist/charat.f89c8860.js
@@ -31371,7 +31371,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56648" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49392" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
